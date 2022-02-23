850f6cafa6b7ed885755b82dab1057bddda64e6b
LackWiz <lackwiztv@gmail.com>
2022-02-23 08:27:55 -0800

    Fixed Windows

--- Rating_Algorithm.py
+++ Rating_Algorithm.py
@@ -7,9 +7,9 @@ import MapDownloader
 import Multi
 #import tkinter as tk
 #from tkinter.filedialog import askdirectory
-#tk.Tk().withdraw()
+# tk.Tk().withdraw()
 
-angleDiv = 90
+angleDiv = 45
 
 combinedArrayScale = 4.069
 
@@ -50,7 +50,7 @@ https://bsmg.wiki/mapping/map-format.html#notes-2
 # 0 = Bottom (you)
 # 1 = Center
 # 2 = Top (me)
-# 
+#
 
 
 # Funcs ------------------------------------------------------ #
@@ -87,7 +87,7 @@ class Bloq:
                 self.forehand = False
             elif ((self.type == 1) & (self.cutDirection in [0, 5])):
                 self.forehand = False
-            
+
         # Non-negoitables, Down and a select diagonal is forehand
         elif self.cutDirection in [1, 6, 7]:
             # 6 = SW Right Hand, 7 = SE Left Hand
@@ -95,7 +95,7 @@ class Bloq:
                 self.forehand = True
             elif ((self.type == 1) & (self.cutDirection in [1, 6])):
                 self.forehand = True
-        
+
         self.calcAngleDiff()
         self.calcPosDiff()
 
@@ -113,18 +113,23 @@ class Bloq:
         if(self.type == 0):  # Left Hand Side to Side Diff
             # LH centered around 2
             if(self.forehand):
-                self.posDiff = [Multi.SIDE_SEMI_MID, Multi.SIDE_EASY, Multi.SIDE_SEMI_MID, Multi.SIDE_MID][self.bloqPos[0]]
+                self.posDiff = [Multi.SIDE_SEMI_MID, Multi.SIDE_EASY,
+                                Multi.SIDE_SEMI_MID, Multi.SIDE_MID][self.bloqPos[0]]
             elif(not self.forehand):
-                self.posDiff = [Multi.SIDE_EASY, Multi.SIDE_SEMI_MID, Multi.SIDE_SEMI_MID, Multi.SIDE_HARD][self.bloqPos[0]]
-            
+                self.posDiff = [Multi.SIDE_EASY, Multi.SIDE_SEMI_MID,
+                                Multi.SIDE_SEMI_MID, Multi.SIDE_HARD][self.bloqPos[0]]
+
         elif(self.type == 1):  # Right Hand
             if(self.forehand):
-                self.posDiff = [Multi.SIDE_MID, Multi.SIDE_SEMI_MID, Multi.SIDE_EASY, Multi.SIDE_SEMI_MID][self.bloqPos[0]]
+                self.posDiff = [Multi.SIDE_MID, Multi.SIDE_SEMI_MID,
+                                Multi.SIDE_EASY, Multi.SIDE_SEMI_MID][self.bloqPos[0]]
             elif(not self.forehand):
-                self.posDiff = [Multi.SIDE_HARD, Multi.SIDE_MID, Multi.SIDE_SEMI_MID, Multi.SIDE_EASY][self.bloqPos[0]]
-        
+                self.posDiff = [Multi.SIDE_HARD, Multi.SIDE_MID,
+                                Multi.SIDE_SEMI_MID, Multi.SIDE_EASY][self.bloqPos[0]]
+
         # Up and Down Diff
-        self.posDiff *= [Multi.VERT_EASY, Multi.VERT_SEMI_MID,Multi.VERT_MID][abs(2 * (not self.forehand) - self.bloqPos[1])]
+        self.posDiff *= [Multi.VERT_EASY, Multi.VERT_SEMI_MID,
+                         Multi.VERT_MID][abs(2 * (not self.forehand) - self.bloqPos[1])]
 
     # TODO: shorten function
     def calcAngleDiff(self):
@@ -175,11 +180,11 @@ class Bloq:
         if(self.angleChangeDiff >= Multi.ANGLE_MID):
             self.stackDiff = self.numNotes**Multi.NUM_NOTE_HARD_POWER
         else:
-            self.stackDiff = self.numNotes**Multi.NUM_NOTE_EASY_POWER 
+            self.stackDiff = self.numNotes**Multi.NUM_NOTE_EASY_POWER
 
-        
+
 def load_song_dat(path):
-    with open(path, encoding = 'utf8') as json_dat:
+    with open(path, encoding='utf8') as json_dat:
         dat = json.load(json_dat)
 
     return dat
@@ -196,10 +201,12 @@ def extractBloqData(songNoteArray):
         if i == 0:
             BloqDataArray.append(Bloq(
                 block["_type"], block["_cutDirection"], [block["_lineIndex"], block["_lineLayer"]], block["_time"], block["_time"] * mspb))
-            BloqDataArray[-1].setForehand(block['_lineLayer'] != 2) #Forehand if note is not in top section
+            # Forehand if note is not in top section
+            BloqDataArray[-1].setForehand(block['_lineLayer'] != 2)
 
         elif (block["_time"] - songNoteArray[i-1]['_time'] <= (dotSliderPrecision if block["_cutDirection"] == 8 else sliderPrecision)
-              and (block['_cutDirection'] in [songNoteArray[i-1]['_cutDirection'], 8])) or (block["_time"] - songNoteArray[i-1]['_time'] <= 1/32):
+              and (block['_cutDirection'] in [songNoteArray[i-1]['_cutDirection'], 8])) or ((block["_time"] - songNoteArray[i-1]['_time'] <= 1/32) 
+              or (True if max(block["_cutDirection"],songNoteArray[i-1]['_cutDirection']) == 8 else abs(cut_direction_index[block['_cutDirection']]-cut_direction_index[songNoteArray[i-1]['_cutDirection']]) <= 45) and (block["_time"] - songNoteArray[i-1]['_time'] <= 1/4)):
 
             # Adds 1 to keep track of how many notes in a single swing
             BloqDataArray[-1].addNote()
@@ -207,26 +214,35 @@ def extractBloqData(songNoteArray):
         else:
             BloqDataArray.append(
                 Bloq(block["_type"], block["_cutDirection"], [block["_lineIndex"], block["_lineLayer"]], block["_time"], 0))
-            
-            if BloqDataArray[-1].cutDirection in [2,3,8] or BloqDataArray[-1].forehand is None: #If Left/Right/Dot, just toggle between forehand/backhand
+
+            # If Left/Right/Dot, just toggle between forehand/backhand
+            if BloqDataArray[-1].cutDirection in [2, 3, 8] or BloqDataArray[-1].forehand is None:
                 BloqDataArray[-1].setForehand(not BloqDataArray[-2].forehand)
 
-            if(BloqDataArray[-1].forehand == BloqDataArray[-2].forehand): #If Hands reset, e.g. bomb resets, bad mapping
+            # If Hands reset, e.g. bomb resets, bad mapping
+            if(BloqDataArray[-1].forehand == BloqDataArray[-2].forehand):
                 if((BloqDataArray[-1].cutDirection != 8) and (BloqDataArray[-2].cutDirection != 8)):
-                    BloqDataArray[-1].angleChange = abs(cut_direction_index[BloqDataArray[-1].cutDirection]-cut_direction_index[BloqDataArray[-2].cutDirection]) #Calculates Angle Change After parity. e.g. up and down is 0, but up to side is 90
+                    # Calculates Angle Change After parity. e.g. up and down is 0, but up to side is 90
+                    BloqDataArray[-1].angleChange = abs(
+                        cut_direction_index[BloqDataArray[-1].cutDirection]-cut_direction_index[BloqDataArray[-2].cutDirection])
             else:
                 if((BloqDataArray[-1].cutDirection != 8) and (BloqDataArray[-2].cutDirection != 8)):
-                    BloqDataArray[-1].angleChange = abs(180-abs(cut_direction_index[BloqDataArray[-1].cutDirection]-cut_direction_index[BloqDataArray[-2].cutDirection])) #Calculates Angle Change After parity. e.g. up and down is 0, but up to side is 90
+                    # Calculates Angle Change After parity. e.g. up and down is 0, but up to side is 90
+                    BloqDataArray[-1].angleChange = abs(180-abs(
+                        cut_direction_index[BloqDataArray[-1].cutDirection]-cut_direction_index[BloqDataArray[-2].cutDirection]))
 
-            BloqDataArray[-1].angleChangeDiff =  min(1+((max(BloqDataArray[-1].angleChange,45)-45)/angleDiv)**2,2)
+            BloqDataArray[-1].angleChangeDiff = min(
+                1+((max(BloqDataArray[-1].angleChange, 45)-45)/angleDiv)**2, 1.5)
             BloqDataArray[-1].calcStackDiff()
             # calculates swingTime in ms and Speed and shoves into class for processing later
-            BloqDataArray[-1].swingTime = (BloqDataArray[-1].time - #Swing time in ms
+            BloqDataArray[-1].swingTime = (BloqDataArray[-1].time -  # Swing time in ms
                                            BloqDataArray[-2].time)*mspb
-            BloqDataArray[-1].swingSpeed = BloqDataArray[-1].swingAngle/BloqDataArray[-1].swingTime #Swing Speed in degrees/ms
-            
-            BloqDataArray[-1].angleChangeTime = BloqDataArray[-1].angleChange/(BloqDataArray[-1].swingTime) #Change in cut angle swing in degrees/millisecond
-            
+            BloqDataArray[-1].swingSpeed = BloqDataArray[-1].swingAngle / \
+                BloqDataArray[-1].swingTime  # Swing Speed in degrees/ms
+
+            # Change in cut angle swing in degrees/millisecond
+            BloqDataArray[-1].angleChangeTime = BloqDataArray[-1].angleChange / \
+                (BloqDataArray[-1].swingTime)
 
             # TODO: move this elsewhere, should be part of processBloqData, not extract
             temp = 0
@@ -245,7 +261,8 @@ def extractBloqData(songNoteArray):
             # Uses a rolling average to judge pattern difficulty
             for j in range(0, patternRollingAverage):
                 if(len(BloqDataArray) >= j+1):
-                    temp += (BloqDataArray[-1*(j+1)].angleDiff*BloqDataArray[-1*(j+1)].posDiff*BloqDataArray[-1*(j+1)].angleChangeDiff*BloqDataArray[-1*(j+1)].stackDiff)
+                    temp += (BloqDataArray[-1*(j+1)].angleDiff*BloqDataArray[-1*(j+1)].posDiff *
+                             BloqDataArray[-1*(j+1)].angleChangeDiff*BloqDataArray[-1*(j+1)].stackDiff)
             # Helps Speed Up the Average Ramp, then does a proper average past staminaRollingAverage/4 and switches to the conventional rolling average after
             if(len(BloqDataArray) < patternRollingAverage/4):
                 BloqDataArray[-1].patternDiff = (temp /
@@ -261,21 +278,25 @@ def extractBloqData(songNoteArray):
 # TODO: misleading function name
 def combineArray(array1, array2):
     combinedArray: list[Bloq] = array1 + array2
-    
-    for i in range(1,len(combinedArray)):
-        combinedArray[i].combinedStamina = math.sqrt(combinedArray[i].stamina**2 + combinedArray[i-1].stamina**2)
-        combinedArray[i].combinedDiff = math.sqrt(combinedArray[i].combinedStamina**Multi.STAMINA_POWER + combinedArray[i].patternDiff**Multi.PATTERN_POWER) * min(math.sqrt(combinedArray[i].combinedStamina**Multi.STAMINA_POWER),combinedArray[i].patternDiff**Multi.PATTERN_POWER)
+
+    for i in range(1, len(combinedArray)):
+        combinedArray[i].combinedStamina = math.sqrt(
+            combinedArray[i].stamina**2 + combinedArray[i-1].stamina**2)
+        combinedArray[i].combinedDiff = math.sqrt(combinedArray[i].combinedStamina**Multi.STAMINA_POWER + combinedArray[i].patternDiff**Multi.PATTERN_POWER) * min(
+            math.sqrt(combinedArray[i].combinedStamina**Multi.STAMINA_POWER), combinedArray[i].patternDiff**Multi.PATTERN_POWER)
 
     combinedArray.sort(key=lambda x: x.time)
 
     # TODO: change from n**2 to sliding window
     for i in range(0, len(combinedArray)):
         temp = 0
-        for j in range(0, min(combinedRollingAverage,i)): # Uses a rolling average to smooth difficulties between the hands
-            temp += combinedArray[i-min(combinedRollingAverage,j)].combinedDiff
-        combinedArray[i].combinedDiffSmoothed = combinedArrayScale*temp/min(combinedRollingAverage,i+1) # 6
-    
-    
+        # Uses a rolling average to smooth difficulties between the hands
+        for j in range(0, min(combinedRollingAverage, i)):
+            temp += combinedArray[i -
+                                  min(combinedRollingAverage, j)].combinedDiff
+        combinedArray[i].combinedDiffSmoothed = combinedArrayScale * \
+            temp/min(combinedRollingAverage, i+1)  # 6
+
     return combinedArray
 
 
@@ -295,7 +316,7 @@ except FileNotFoundError:
 finally:
     f.close()
 
-#possible overlapping hashes bug
+# possible overlapping hashes bug
 print('Enter song ID:')
 song_id = input()
 
@@ -319,7 +340,8 @@ if not songFound:
         exit()
 
 difficulties = os.listdir(bsPath + "/" + songFolder)
-difficulties = list(filter(lambda x : x.endswith(".dat") and x.lower() != "info.dat", difficulties))
+difficulties = list(filter(lambda x: x.endswith(
+    ".dat") and x.lower() != "info.dat", difficulties))
 
 print("Select a difficulty: ")
 for i in range(0, len(difficulties)):
@@ -328,7 +350,7 @@ for i in range(0, len(difficulties)):
 while (diff := int(input())) <= 0 or diff > len(difficulties):
     print(f"Input not in range 1-{len(difficulties)}, try again")
 
-song_diff = difficulties[diff - 1] 
+song_diff = difficulties[diff - 1]
 
 #---------------Where Stuff Happens-----------------------------------------------------------------------------#
 
@@ -352,18 +374,18 @@ combinedArrayRaw = combineArray(BloqDataLeft, BloqDataRight)
 # export results to spreadsheet
 excelFileName = os.path.join(
     f"Spreadsheets/{song_id} {song_info['_songName']} {song_diff} export.csv")
-
+excelFileName.replace("*", "")
 try:
     f = open(excelFileName, 'w', newline="")
 except FileNotFoundError:
     print('Making Spreadsheets Folder')
     os.mkdir('Spreadsheets')
     f = open(excelFileName, 'w', newline="")
-    
+
 finally:
     writer = csv.writer(f)
-    writer.writerow(["TimeMS","Beat","Type","Forehand","numNotes", "SwingSpeed", "Angle Diff", "AngleChangeDiff","Pos Diff",
-                    "C Stamina", "C Pattern Diff", "C CombinedDiff", "C SmoothedDiff"])
+    writer.writerow(["TimeMS", "Beat", "Type", "Forehand", "numNotes", "SwingSpeed", "Angle Diff", "AngleChangeDiff", "Pos Diff",
+                    "Stamina", "Pattern Diff", "CombinedDiff", "SmoothedDiff"])
     for bloq in combinedArrayRaw:
         writer.writerow([bloq.timeMS, bloq.time,  bloq.type, bloq.forehand, bloq.numNotes, bloq.swingSpeed, bloq.angleDiff, bloq.angleChangeDiff, bloq.posDiff,
                         bloq.combinedStamina, bloq.patternDiff, bloq.combinedDiff, bloq.combinedDiffSmoothed])
@@ -373,12 +395,14 @@ finally:
 combinedArray = [bloq.combinedDiffSmoothed for bloq in combinedArrayRaw]
 
 combinedArray.sort(reverse=True)
-top_1_percent = sum(combinedArray[:int(len(combinedArray)/100)])/int(len(combinedArray)/100)
+top_1_percent = sum(
+    combinedArray[:int(len(combinedArray)/100)])/int(len(combinedArray)/100)
 
 median = statistics.median(combinedArray)
 final_score = (top_1_percent*7 + median*3)/10
 
-print(final_score)
+print("Weighted Score:" + str(final_score))
+print("Median:" + str(median))
 print("Press Enter to Exit!")
 input()
 
