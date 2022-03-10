41520744b30b80de2f1f3922d019211ad48499e0
n1ckname152 <38827730+n1ckname152@users.noreply.github.com>
2022-03-09 16:33:39 -0800

    Update Rating_Algorithm.py

--- Rating_Algorithm.py
+++ Rating_Algorithm.py
@@ -192,7 +192,7 @@ def findSongFolder(song_id):
     song_options = os.listdir(bsPath)
     songFound = False
     for song in song_options:
-        if song.startswith(song_id+" ") != False:
+        if song.startswith(song_id+" "):
             songFolder = song
             songFound = True
             break
